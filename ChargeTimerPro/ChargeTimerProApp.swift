8d0f7a9cc7c723cbcee18e90a111963c01236cc6
Alex Harris <alexharris0079@gmail.com>
2025-12-01 05:56:40 +1000

    Initial commit – full ChargeTimer Pro project from final blueprint

---
+++ ChargeTimerPro/ChargeTimerProApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChargeTimerProApp.swift
+//  ChargeTimerPro
+//
+//  Created by Alex Harris on 1/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChargeTimerProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChargeTimerPro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChargeTimerPro
+//
+//  Created by Alex Harris on 1/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
